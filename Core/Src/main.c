2b5b1e1f1c2c03beaf725bdd44bd209bff128b6d
sq4250 <3535687466@qq.com>
2023-12-21 22:19:45 +0800

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,10 +3,10 @@
 int main(){
     _32bit *chessman = (_32bit *)calloc(16, 4);
     do{
-    menu(chessman);
-    icon(chessman);
-    printf("\n>>> press any key...");
-    getch();
+        menu(chessman);
+        icon(chessman);
+        printf("\n>>> press any key...");
+        getch();
     } while (1);
     return 0;
 }
