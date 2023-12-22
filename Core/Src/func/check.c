3b8473ee8b97b704636afd249c6024a8eea027a0
sq4250 <3535687466@qq.com>
2023-12-22 18:05:40 +0800

    -d

--- Core/Src/func/check.c
+++ Core/Src/func/check.c
@@ -16,32 +16,24 @@ int checkchess(_32bit *a, int c){
     }
     
     for (int i = 1; i < 16; i++)
-        for (int j = 0; j < 16; j++){
+        for (int j = 0; j < 16; j++)
             if (((a[i] & (0xC0000000 >> (j * 2))) >> (30 - j * 2)) == c)
                 p[0][i][j] = p[0][i - 1][j] + 1;
-            else p[0][i][j] = 0;
-        }
     
     for (int i = 0; i < 16; i++)
-        for (int j = 1; j < 16; j++){
+        for (int j = 1; j < 16; j++)
             if (((a[i] & (0xC0000000 >> (j * 2))) >> (30 - j * 2)) == c)
                 p[1][i][j] = p[1][i][j - 1] + 1;
-            else p[1][i][j] = 0;
-        }
     
     for (int i = 1; i < 16; i++)
-        for (int j = 1; j < 16; j++){
+        for (int j = 1; j < 16; j++)
             if (((a[i] & (0xC0000000 >> (j * 2))) >> (30 - j * 2)) == c)
                 p[2][i][j] = p[2][i - 1][j - 1] + 1;
-            else p[2][i][j] = 0;
-        }
     
     for (int i = 14; i >= 0; i--)
-        for (int j = 1; j < 16; j++){
+        for (int j = 1; j < 16; j++)
             if (((a[i] & (0xC0000000 >> (j * 2))) >> (30 - j * 2)) == c)
                 p[3][i][j] = p[3][i + 1][j - 1] + 1;
-            else p[3][i][j] = 0;
-        }   
     
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 16; j++)
