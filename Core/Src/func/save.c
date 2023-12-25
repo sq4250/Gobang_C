c6631aa7c849c59ee27989b6a42606957af2bea0
sq4250 <138225630+sq4250@users.noreply.github.com>
2023-12-25 20:51:12 +0800

    Delete Core/Src directory

--- Core/Src/func/save.c
+++
@@ -1,8 +0,0 @@
-#include "Inc.h"
-void save(_32bit *chessman){
-    FILE *fp = NULL;
-    fp = fopen(".data.txt", "w");
-    for (int i = 0; i < 16; i++)
-        fprintf(fp, "%x\n", chessman[i]);
-    fclose(fp);
-}
