3ae287611190eb9bdf0e3b773456da11c0bf9775
sq4250 <3535687466@qq.com>
2023-12-25 14:30:33 +0800

    update

--- Core/Src/func/ai.c
+++ Core/Src/func/ai.c
@@ -1,25 +1,130 @@
 #include "Inc.h"
-#define DEPTH 3
-#define AREA 3
+#define DEPTH 2
+#define AREA 1
 typedef struct NODE{
-    int i, j, sup, inf, depth;
+    int i, sup, inf, depth;
     _32bit chessman[15];
-    struct NODE *sibling;
-    struct NODE *child;
+    struct NODE *parent, *sibling, *child;
 } NODE;
 int area(int a){
     if (a < 0) return 0;
     else if (a > 14) return 14;
     else return a;
 }
-void evaluate(NODE *node);
-void cut();
-void update();
+int score(int a){
+    switch (a){
+        case 0x200:
+        case 0x80:
+        case 0x20:
+        case 0x8:
+        case 0x2:
+            return 1;
+        case 0x280:
+        case 0xA0:
+        case 0x28:
+        case 0xA:
+        case 0x202:
+        case 0x82:
+        case 0x22:
+        case 0x208:
+        case 0x88:
+        case 0x220:
+            return 10;
+        case 0xA8:
+        case 0x228:
+        case 0x288:
+        case 0x2A0:
+        case 0xA2:
+        case 0x222:
+        case 0x282:
+        case 0x8A:
+        case 0x20A:
+        case 0x2A:
+            return 100;
+        case 0xAA:
+        case 0x2A8:
+        case 0x2A2:
+        case 0x28A:
+        case 0x22A:
+            return 10000;
+        case 0x2AA:
+            return 1000000;
+        case 0x100:
+        case 0x40:
+        case 0x10:
+        case 0x4:
+        case 0x1:
+            return -1;
+        case 0x5:
+        case 0x11:
+        case 0x41:
+        case 0x101:
+        case 0x104:
+        case 0x44:
+        case 0x14:
+        case 0x110:
+        case 0x50:
+        case 0x140:
+            return -10;
+        case 0x54:
+            return -2000;
+        case 0x114:
+        case 0x144:
+        case 0x150:
+        case 0x51:
+        case 0x111:
+        case 0x141:
+        case 0x45:
+        case 0x105:
+        case 0x15:
+            return -1000;
+        case 0x154:
+        case 0x55:
+        case 0x151:
+        case 0x145:
+        case 0x115:
+            return -100000;
+        case 0x155:
+            return -10000000;
+        default :
+            return 0; 
+    }
+}
+int add(NODE *node){
+    int s, m = 0, a = 0xC0000000, b = 0xF8000000;
+    for (int i = 0; i < 11; i++)
+        for (int j = 0; j < 15; j++){
+            s = ((node->chessman[i] & (a >> j * 2)) >> (30 - j * 2 -8)) + ((node->chessman[i + 1] & (a >> j * 2)) >> (30 - j * 2 -6)) + ((node->chessman[i + 2] & (a >> j * 2)) >> (30 - j * 2 -4)) + ((node->chessman[i + 3] & (a >> j * 2)) >> (30 - j * 2 -2)) + ((node->chessman[i + 4] & (a >> j * 2)) >> (30 - j * 2));
+            m += score(s);
+        }
+    
+    for (int i = 0; i < 15; i++)
+        for (int j = 0; j < 11; j++){
+            s = ((node->chessman[i] & (b >> j * 2)) >> (20 - j * 2));
+            m += score(s);
+        }
+
+    for (int i = 0; i < 11; i++)
+        for (int j = 0; j < 11; j++){
+            s = ((node->chessman[i] & (a >> j * 2)) >> (30 - j * 2 -8)) + ((node->chessman[i + 1] & (a >> j * 2 +2)) >> (30 - j * 2 -6 -2)) + ((node->chessman[i + 2] & (a >> j * 2 +4)) >> (30 - j * 2 -4 -4)) + ((node->chessman[i + 3] & (a >> j * 2 +6)) >> (30 - j * 2 -2 -6)) + ((node->chessman[i + 4] & (a >> j * 2 +8)) >> (30 - j * 2 -8));
+            m += score(s);
+        }
+
+    for (int i = 10; i >= 0; i--)
+        for (int j = 0; j < 11; j++){
+            s = ((node->chessman[i] & (a >> j * 2 + 8)) >> (30 - j * 2 -8 -8)) + ((node->chessman[i + 1] & (a >> j * 2 +6)) >> (30 - j * 2 -6 -6)) + ((node->chessman[i + 2] & (a >> j * 2 +4)) >> (30 - j * 2 -4 -4)) + ((node->chessman[i + 3] & (a >> j * 2 +2)) >> (30 - j * 2 -2 -2)) + ((node->chessman[i + 4] & (a >> j * 2)) >> (30 - j * 2));
+            m += score(s);
+        }
+    
+    return m;
+}
+
+
 void search(NODE *node){
     NODE *sibling = (NODE *)malloc(sizeof(NODE)), *head = sibling;
     for (int i = 0; i < 15; i++)
         for (int j = 0; j < 15; j++)
-            if (((node->chessman[i] & (0xC0000000 >> (j * 2))) >> (30 - j * 2)) != 0)
+            if (((node->chessman[i] & (0xC0000000 >> j * 2)) >> (30 - j * 2)) != 0)
                 for (int ii = area(i - AREA); ii <= area(i + AREA); ii++)
                     for (int jj = area(j - AREA); jj <= area(j + AREA); jj++)
                         if (((node->chessman[ii] & (0xC0000000 >> (jj * 2))) >> (30 - jj * 2)) == 0){
@@ -27,35 +132,87 @@ void search(NODE *node){
                             sibling = sibling->sibling;
                             sibling->depth = node->depth + 1;
                             sibling->i = ii;
-                            sibling->j = jj;
                             sibling->child = NULL;
                             for (int i = 0; i < 15; i++)
                                 sibling->chessman[i] = node->chessman[i];
-                            if ((sibling->depth) % 2 == 0)
+                            if (sibling->depth % 2 == 0)
                                 sibling->chessman[ii] |= (0x40000000 >> (jj * 2));
-                            else if ((sibling->depth) % 2 == 1)
+                            else if (sibling->depth % 2 == 1)
                                 sibling->chessman[ii] |= (0x80000000 >> (jj * 2));
                         }
     sibling->sibling = NULL;
     node->child = head->sibling;
     free(head);
 }
-void NLR(NODE *node, int i){
-    if (node == NULL) return;
-    else if (node->depth == i){
-        search(node);
-        NLR(node->sibling, i);
-        NLR(node->child, i);
+void nodes(NODE *node, int depth){
+    if (node != NULL){
+        if (node->depth == depth)
+            search(node);
+        nodes(node->sibling, depth);
+        nodes(node->child, depth);
+    } 
+}
+void parent(NODE *node){
+    if (node != NULL){
+        if (node->child != NULL) node->child->parent = node;
+        if (node->sibling != NULL) node->sibling->parent = node->parent;
+        parent(node->child);
+        parent(node->sibling);
+    }
+}
+void evaluate(NODE *node){
+    if (node != NULL){
+        if (node->depth == DEPTH)
+            node->inf = node->sup = add(node);
+        evaluate(node->sibling);
+        evaluate(node->child);
+    }
+}
+void find(NODE *node){
+    if (node != NULL){
+        if (node->depth != DEPTH && node->depth != 0){
+            node->inf = node->parent->inf;
+            node->sup = node->parent->sup;
+        }
+        find(node->child);
+        if (node->depth != 0){
+            if (node->depth % 2 == 0 && node->inf < node->parent->sup)node->parent->sup = node->inf;
+            else if (node->depth % 2 == 1 && node->sup > node->parent->inf)node->parent->inf = node->sup;
+            if (node->parent->sup > node->parent->inf) find(node->sibling);
+        }
+    }
+}
+void free_tree(NODE *node){
+    if (node != NULL){
+        if (node->child != NULL){
+            free_tree(node->child);
+            node->child = NULL;
+        }
+        if (node->sibling != NULL){
+            free_tree(node->sibling);
+            node->sibling = NULL;
+        }
+        free(node);
     }
 }
 int ai(_32bit *chessman){
-    NODE *root;
-    root = (NODE *)malloc(sizeof(NODE));
+    NODE *root = (NODE *)malloc(sizeof(NODE));
     root->depth = 0;
-    root->sibling = NULL;
+    root->parent = root->sibling = root->child = NULL;
+    root->inf = 0x80000000;
+    root->sup = 0x7fffffff;
     for (int i = 0; i < 15; i++)
         root->chessman[i] = chessman[i];
     for (int i = 0; i < DEPTH; i++)
-        NLR(root, i);
+        nodes(root, i);
+    parent(root);
+    evaluate(root);
+    find(root);
+    for (NODE *sibling = root->child; sibling != NULL; sibling = sibling->sibling)
+        if (sibling->sup == root->inf){
+            chessman[sibling->i] = sibling->chessman[sibling->i];
+            break;
+        }
+    free_tree(root);
     return -1;
 }
